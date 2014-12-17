183f58c5d706ab3a197e571897e611e6b4d21f38
ŞEYHMUS ERBEKLER <seyhmuserbekler@hotmail.com>
2014-12-17 16:14:51 +0200

    test

---
+++ OtelOtomasyonuProjesi/OtelOtomasyonuProjesi/Form2.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace OtelOtomasyonuProjesi
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.müsterilerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.müsteriEkleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.müsteriGüncelleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.müsteriSilToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.odalarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.odaEkleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.odaGüncelleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.odaSilToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(1, 385);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Boş Odalar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(145, 385);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(162, 34);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Dolu Odalar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(301, 385);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(141, 34);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Müşteri Ara";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.müsterilerToolStripMenuItem,
+            this.odalarToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(444, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // müsterilerToolStripMenuItem
+            // 
+            this.müsterilerToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.müsteriEkleToolStripMenuItem,
+            this.müsteriGüncelleToolStripMenuItem,
+            this.müsteriSilToolStripMenuItem});
+            this.müsterilerToolStripMenuItem.Name = "müsterilerToolStripMenuItem";
+            this.müsterilerToolStripMenuItem.Size = new System.Drawing.Size(72, 20);
+            this.müsterilerToolStripMenuItem.Text = "Müsteriler";
+            // 
+            // müsteriEkleToolStripMenuItem
+            // 
+            this.müsteriEkleToolStripMenuItem.Name = "müsteriEkleToolStripMenuItem";
+            this.müsteriEkleToolStripMenuItem.Size = new System.Drawing.Size(163, 22);
+            this.müsteriEkleToolStripMenuItem.Text = "Müsteri Ekle";
+            // 
+            // müsteriGüncelleToolStripMenuItem
+            // 
+            this.müsteriGüncelleToolStripMenuItem.Name = "müsteriGüncelleToolStripMenuItem";
+            this.müsteriGüncelleToolStripMenuItem.Size = new System.Drawing.Size(163, 22);
+            this.müsteriGüncelleToolStripMenuItem.Text = "Müsteri Güncelle";
+            // 
+            // müsteriSilToolStripMenuItem
+            // 
+            this.müsteriSilToolStripMenuItem.Name = "müsteriSilToolStripMenuItem";
+            this.müsteriSilToolStripMenuItem.Size = new System.Drawing.Size(163, 22);
+            this.müsteriSilToolStripMenuItem.Text = "Müsteri Sil";
+            // 
+            // odalarToolStripMenuItem
+            // 
+            this.odalarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.odaEkleToolStripMenuItem,
+            this.odaGüncelleToolStripMenuItem,
+            this.odaSilToolStripMenuItem});
+            this.odalarToolStripMenuItem.Name = "odalarToolStripMenuItem";
+            this.odalarToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.odalarToolStripMenuItem.Text = "Odalar";
+            // 
+            // odaEkleToolStripMenuItem
+            // 
+            this.odaEkleToolStripMenuItem.Name = "odaEkleToolStripMenuItem";
+            this.odaEkleToolStripMenuItem.Size = new System.Drawing.Size(145, 22);
+            this.odaEkleToolStripMenuItem.Text = "Oda Ekle";
+            // 
+            // odaGüncelleToolStripMenuItem
+            // 
+            this.odaGüncelleToolStripMenuItem.Name = "odaGüncelleToolStripMenuItem";
+            this.odaGüncelleToolStripMenuItem.Size = new System.Drawing.Size(145, 22);
+            this.odaGüncelleToolStripMenuItem.Text = "Oda Güncelle";
+            // 
+            // odaSilToolStripMenuItem
+            // 
+            this.odaSilToolStripMenuItem.Name = "odaSilToolStripMenuItem";
+            this.odaSilToolStripMenuItem.Size = new System.Drawing.Size(145, 22);
+            this.odaSilToolStripMenuItem.Text = "Oda Sil";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Pink;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(444, 418);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem müsterilerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem müsteriEkleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem müsteriGüncelleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem müsteriSilToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem odalarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem odaEkleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem odaGüncelleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem odaSilToolStripMenuItem;
+    }
+}
